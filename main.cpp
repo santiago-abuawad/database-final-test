0756ab9d0e8f55c0952b9b35b09fb7ee1b89eac3
Santiago Abuawad <85538772+santiago-abuawad@users.noreply.github.com>
2024-06-04 22:59:13 -0400

    estructurafecha

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 #include <vector>
 #include <map>
 
+using namespace std;
+
+//Estructura-----------------------------------------------------
+struct Fecha {
+    int anio;
+    int mes;
+    int dia;
+    string Fecha_De_Entrada;
+};
+
+//Funciones-----------------------------------------------------
+
+//-----------------------------------------------------
 int main() {
     return 0;
 }
